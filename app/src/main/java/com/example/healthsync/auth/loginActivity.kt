3739a64d5d7281b6744ef5373d8c068148968e56
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-01-09 13:19:38 +0530

    Basic setup of project and ui design of auth and splash screen

---
+++ app/src/main/java/com/example/healthsync/auth/loginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.healthsync.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.healthsync.R
+
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
