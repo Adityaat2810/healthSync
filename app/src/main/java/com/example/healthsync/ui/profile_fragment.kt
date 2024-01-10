c426534c4611d9da131f54618a91accb573fcd23
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-01-10 13:55:38 +0530

    set up the ui for Home Fragment

--- app/src/main/java/com/example/healthsync/ui/profile_fragment.kt
+++ app/src/main/java/com/example/healthsync/ui/profile_fragment.kt
@@ -1,60 +1,56 @@
 package com.example.healthsync.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.healthsync.R
+import com.example.healthsync.auth.loginActivity
+import com.example.healthsync.databinding.FragmentProfileFragmentBinding
+import com.example.healthsync.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [profile_fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class profile_fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentProfileFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_fragment, container, false)
-    }
+        binding = FragmentProfileFragmentBinding.inflate(layoutInflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment profile_fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            profile_fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+        FirebaseDatabase.getInstance().getReference("users")
+            .child(FirebaseAuth.getInstance().currentUser!!.uid!!).get()
+            .addOnSuccessListener {
+                if(it.exists()){
+                    val data  = it.getValue(UserModel::class.java)
+                    binding.userName.setText(data!!.name.toString())
+                    binding.userCity.setText(data!!.city.toString())
+                    binding.userEmail.setText(data!!.email.toString())
+                    binding.userNumber.setText(data!!.number.toString())
+
+                    Glide.with(this).load(data.image)
+                        .placeholder(R.drawable.profile).into(binding.userPic)
                 }
             }
+
+        binding.logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(requireContext(),loginActivity::class.java))
+            requireActivity().finish()
+
+        }
+
+
+        return binding.root
+
     }
+
+
 }
